345c71e2643435b505c9120866ee2935fc2b8797
Jason <jadomican@gmail.com>
2018-02-15 01:42:15 +0000

    Lab3

--- Lab3/Lab3/main.cpp
+++ Lab3/Lab3/main.cpp
@@ -1,35 +1,94 @@
 //X00119321 Jason Domican Lab3
 
-#include <iostream>
+/***************************************************************************************
+*    Usage: modified
+*    Title: Merge sort
+*    Date: 14/02/2018
+*    Availability: http://www.cplusplus.com/forum/general/166536/
+***************************************************************************************/
 
+#include <iostream>
 using namespace std;
 
-void mergeSort(int arrayToSort[], int startIndex, int lengthToSort)
-{
-	if (startIndex < lengthToSort)
-	{
-		int m = startIndex + (lengthToSort - 1) / 2;
-
-		mergeSort(arrayToSort, 1, m);
-		mergeSort(arrayToSort, m + 1, lengthToSort);
-		//merge(arrayToSort, 1, lengthToSort);
-	}
-}
-
+// Merge Sort
 void merge(int arraySortedInTwoHalves[], int startIndex, int length)
 {
 
+	int size = (length - startIndex) + 1;
+	int *b = new int[size]();
+
+	int i = startIndex;
+	int mid = (startIndex + length) / 2;
+	int k = 0;
+	int j = mid + 1;
+
+	while (k < size)
+	{
+		if ((i <= mid) && (arraySortedInTwoHalves[i] < arraySortedInTwoHalves[j]))
+		{
+			b[k++] = arraySortedInTwoHalves[i++];
+		}
+		else
+		{
+			b[k++] = arraySortedInTwoHalves[j++];
+		}
+
+	}
+
+	for (k = 0; k < size; k++)
+	{
+		arraySortedInTwoHalves[startIndex + k] = b[k];
+	}
+
+	delete[]b;
+
 }
+
+//The recursive merge sort function
+void mergeSort(int arrayToSort[], int startIndex, int endIndex)
+{
+	int midIndex;
+
+	//Check for base case
+	if (startIndex >= endIndex)
+	{
+		return;
+	}
+
+	//First, divide in half
+	midIndex = (startIndex + endIndex) / 2;
+
+	//First recursive call
+	mergeSort(arrayToSort, startIndex, midIndex);
+	//Second recursive call
+	mergeSort(arrayToSort, midIndex + 1, endIndex);
+	//The merge function
+	merge(arrayToSort, startIndex, endIndex);
+
+}
+
+
+//Quicksort
+int quickSortDivide(int *theArray, int size) 
+{
+
+	
+}
+
+
 
 
 int main()
 {
+	int array[10] = { 2,4,3,5,7,2,8,3,6,10 };
 
+	mergeSort(array, 0, 9);
 
-	cout << "\n\n";
+	for (int i = 0; i < 10; i++)
+	{
+		cout << array[i] << " ";
+	}
+
 	system("pause");
 	return 0;
 }
-
-
-//ca - quicksort
